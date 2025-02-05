2924ef3818764983bf9772c96561a63a7287ef2f
Arthur <arthurlucas92@orange.fr>
2025-02-05 17:28:01 +0100

    route getRestaurants fonctionne

--- amplify/backend/function/getRestaurants/src/index.py
+++ amplify/backend/function/getRestaurants/src/index.py
@@ -1,15 +1,27 @@
 import json
+import boto3
+import os
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table(os.environ["STORAGE_RESTAURANT_NAME"])
 
 def handler(event, context):
-  print('received event:')
-  print(event)
-  
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
-  }
+    try:
+        # Scan pour récupérer tous les restaurants
+        response = table.scan()
+        restaurants = response.get("Items", [])
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Headers": "*",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            "body": json.dumps(restaurants, default=str)  # Conversion des données en JSON
+        }
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": str(e)})
+        }
